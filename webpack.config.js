63e97612909fe3fecbd2f7c6207a35190e26a9eb
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-10-17 18:57:36 -0500

    add contenthash

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, './dist'),
     publicPath: 'dist/'
   },
@@ -57,7 +57,7 @@ module.exports = {
   plugins: [
     new TerserPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'style.css'
+      filename: 'style.[contenthash].css'
     })
   ]
 }
