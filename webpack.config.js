ddae3d38bdb92df9fae7b3f5bcc4c4db4e596a93
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-10-17 18:37:10 -0500

    add loader, babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,51 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist')
+    path: path.resolve(__dirname, './dist'),
+    publicPath: 'dist/'
   },
   mode: 'none',
   module: {
     rules: [
       {
         test: /\.(ttf)$/,
-        type: 'asset/resource'
+        type: 'asset'
       },
       {
         test: /\.(png|jpg)$/,
-        type: 'asset/resource'
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024 // 3 kilobytes
+          }
+        }
+      },
+      {
+        test: /\.txt$/,
+        type: 'asset/source'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', 'css-loader', 'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
       }
     ]
   }
